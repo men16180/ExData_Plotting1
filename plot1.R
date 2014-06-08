6954fdfce5fd8efbe4969cbba74f3e1070cd09fa
men16180 <men16180@users.noreply.github.com>
2014-06-08 11:14:11 +1000

    updated Plot1.R to fix axes and added new plot1.png

--- plot1.R
+++ plot1.R
@@ -19,10 +19,14 @@ data$Time <- strptime(paste(data$Date,data$Time,sep=" "),
 data$Date <- as.Date(data$Date, "%d/%m/%Y")
 
 # Create histogram plot and write to Plot1.png in directory "figure"
-png(file = "./figure/Plot1.png", width = 480, height = 480, units = "px",bg = "transparent") 
-with(subset(data, (data$Date=="2007-02-01" | data$Date=="2007-02-02") ),
-     hist(Global_active_power, ylim=c(0,1200), 
+png(file = "./figure/Plot1.png", width = 480, height = 480, units = "px", bg = "transparent") 
+with(subset(data, (data$Date=="2007-02-01" | data$Date=="2007-02-02") ), {
+        hist(Global_active_power, ylim=c(0,1200), 
           breaks=seq(from = 0, to = 8, by = 0.5),
           xlim=c(0,6), xlab="Global active power (kilowatts)",
-          col="red", main="Global Active Power"))
+          col="red", main="Global Active Power", axes=FALSE)
+        axis(side = 1, at = c(0,2,4,6))
+        axis(side = 2, at = c(0,200,400,600,800,1000,1200))
+        box()
+     })
 dev.off()
